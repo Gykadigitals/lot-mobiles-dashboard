29408fb210a2a403244d8e79e4dd71e74428a6dd
Gykadigitals <gykadigitals@gmail.com>
2026-06-30 19:03:04 +0530

    refactor: update SEO metadata title and description to LOT Mobiles across all pages

--- src/app/(full-width-pages)/(error-pages)/error-404/page.tsx
+++ src/app/(full-width-pages)/(error-pages)/error-404/page.tsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 import React from "react";
 
 export const metadata: Metadata = {
-  title: "Next.js Error 404 | TailAdmin - Next.js Dashboard Template",
+  title: "LOT Mobiles",
   description:
-    "This is Next.js Error 404 page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+    "LOT Mobiles",
 };
 
 export default function Error404() {
